bfed4fda128ac44b969648fd04ec484ff5c80238
Githubbhanu <58077984+Githubbhanu@users.noreply.github.com>
2021-10-10 21:25:59 +0530

    Pattern Problem Solution Assignment

---
+++ Day2_Pattern01.java
@@ -0,0 +1,31 @@
+/**
+Write code to print the following pattern:
+
+1
+
+0 1
+
+1 0 1
+
+0 1 0 1
+
+1 0 1 0 1
+**/
+import java.util.Scanner;
+public class Solution{
+	public static void main(String []args){
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		for(int i=0; i<n; i++){
+			for(int j=0; j<=i; j++){
+				if((i+j)%2 == 0){
+					System.out.print("1 ");
+				}else{
+					System.out.print("0 ");
+				}
+			}
+			System.out.println();
+		}
+	
+	}
+}
